62f2501a1efe46b451f0aa65821dbbbf3dee492a
Nechaev Victor <vitanecaev373@gmail.com>
2024-10-20 10:11:04 +0300

    1 addition

---
+++ laba3.1/main.py
@@ -0,0 +1,13 @@
+with open('fileint.txt', 'r') as file:
+    content = file.read()
+    numbers = content.split()
+
+for number in numbers:
+    if number.isdigit():  # verification natural numbers
+        print(f"Число: {number}")
+        digit_count = len(number) #digit count
+        print(f"Цифры числа: {' '.join(number)}")  #split number
+        print(f"Количество цифр: {digit_count}")
+    else:
+        print(f"{number} не является натуральным числом.")
+
