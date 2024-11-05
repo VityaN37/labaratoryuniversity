5b99d2ec07cf0fcd6d7c1210bfbcc7b45b0ff3db
Nechaev Victor <vitanecaev373@gmail.com>
2024-11-05 12:54:45 +0300

    seven

--- laba4.1/main.py
+++ laba4.1/main.py
@@ -2,6 +2,16 @@
 import re
 with open('file.txt', 'r') as file:
     content = file.read()
-j=r'\d[1-9]'
+j=r'\s[1-9]\d*'
 numbers = re.findall(j, content)
+
 print(numbers)
+
+for number in numbers:
+        print(f"Число: {number}")
+        digit_count = {} #digit count
+        for i in number:
+            digit_count[i] = digit_count.get(i, 0) + 1
+        print(f"Количество цифр: {digit_count}")
+
+
