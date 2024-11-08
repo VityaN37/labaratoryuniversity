32a2b47f6754c95487f8b1ed3d0e5d4f5df24402
Nechaev Victor <vitanecaev373@gmail.com>
2024-11-08 10:12:58 +0300

    seven

--- laba4.1/main.py
+++ laba4.1/main.py
@@ -1,11 +1,15 @@
 #Натуральные числа. Для каждого числа вывести используемые цифры (прописью) и их количество
 import re
 with open('file.txt', 'r') as file:
-    content = file.read()
-j=r'\s[1-9]\d*'
-numbers = re.findall(j, content)
+    #numbers = file.readline()
+    content = file.readline()
 
+#j=r'\b[1-9][0-9]*\b'
+#j=r'[1-9]\d*[^.|^\s]'
+numbers = re.findall(r"\b[1-9]+[^.]", content)
 print(numbers)
+#l=re.findall(r"\b[^-][1-9]\d*",numbers)
+#print(l)
 
 for number in numbers:
         print(f"Число: {number}")
